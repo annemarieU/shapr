193b321a66c7afef4b93344a69922bb658d061c4
Annemarie <95631257+annemarieU@users.noreply.github.com>
2024-05-18 21:40:52 +0200

    Update install_r_packages.R

--- python/install_r_packages.R
+++ python/install_r_packages.R
@@ -1,4 +1,4 @@
 # Installs the required R-packages
-install.packages("remotes", repos = "https://cloud.r-project.org")
-remotes::install_github("NorskRegnesentral/shapr")
+install.packages("remotes", repos = "https://cloud.r-project.org", lib = "/usr/lib/R/site-library/", force=TRUE, clean = TRUE, quiet = FALSE)
+remotes::install_github("annemarieU/shapr", force= TRUE)
 # Installs the development version of shapr from the master branch on CRAN
